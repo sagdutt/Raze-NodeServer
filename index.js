301fc2ebdd2a94ec3d417612b131455830ec7e69
Sagnik Dutta <sagdutt@gmail.com>
2021-05-02 21:06:12 +0530

    Fixing start command

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // Events
 const CONNECTION = 'connection';
